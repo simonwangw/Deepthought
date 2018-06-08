2e1ea0b89e935427d1e2b500f88e9f762cdcdc76
wangwei <wangweitl81@hotmail.com>
2018-06-08 16:19:46 +0800

    update

--- Deepthought/src/test/java/org/telegram/channel/GetAllChatsTest.java
+++ Deepthought/src/test/java/org/telegram/channel/GetAllChatsTest.java
@@ -2,11 +2,14 @@ package org.telegram.channel;
 
 import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
+import org.telegram.api.chat.TLAbsChat;
+import org.telegram.api.chat.TLChat;
 import org.telegram.api.engine.RpcException;
 import org.telegram.api.functions.messages.TLRequestMessagesGetAllChats;
 import org.telegram.api.messages.chats.TLAbsMessagesChats;
 import org.telegram.framework.AbstractTest;
 import org.telegram.tl.TLIntVector;
+import org.telegram.tl.TLVector;
 
 import java.util.concurrent.ExecutionException;
 
@@ -24,7 +27,11 @@ public class GetAllChatsTest extends AbstractTest {
         try {
             TLAbsMessagesChats tlMessagesChats = this.getKernelComm().doRpcCallSync(tlRequestMessagesGetAllChats);
             System.out.println(JSONObject.toJSONString(tlMessagesChats));
-
+            TLVector<TLAbsChat> chatList = tlMessagesChats.getChats();
+            for (TLAbsChat chat : chatList) {
+                TLChat c = (TLChat)chat;
+                System.out.println(c.getTitle());
+            }
             //tlMessagesChats.getChats().get(0).getId();
         } catch (ExecutionException e) {
             e.printStackTrace();
