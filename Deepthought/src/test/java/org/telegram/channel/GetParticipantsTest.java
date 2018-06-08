91287f24b57c15511d9ba13ad3be5f3347db0df4
wangwei <wangweitl81@hotmail.com>
2018-06-08 15:50:09 +0800

    update

---
+++ Deepthought/src/test/java/org/telegram/channel/GetParticipantsTest.java
@@ -0,0 +1,39 @@
+package org.telegram.channel;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+import org.telegram.api.channel.TLChannelParticipants;
+import org.telegram.api.engine.RpcException;
+import org.telegram.api.functions.channels.TLRequestChannelsGetParticipants;
+import org.telegram.api.input.chat.TLInputChannel;
+import org.telegram.framework.AbstractTest;
+
+import java.util.concurrent.ExecutionException;
+
+public class GetParticipantsTest extends AbstractTest {
+
+    @Test
+    public void testGetParticipants() {
+        TLRequestChannelsGetParticipants tlRequestChannelsGetParticipants
+                = new TLRequestChannelsGetParticipants();
+        TLInputChannel tlAbsInputChannel = new TLInputChannel();
+        tlAbsInputChannel.setChannelId(1160021388);
+        tlAbsInputChannel.setAccessHash(6598956788439284138L);
+        tlRequestChannelsGetParticipants.setChannel(tlAbsInputChannel);
+        tlRequestChannelsGetParticipants.setFilter(null);
+        tlRequestChannelsGetParticipants.setLimit(100);
+        tlRequestChannelsGetParticipants.setOffset(0);
+
+        try {
+            TLChannelParticipants tlChannelParticipants = this.getKernelComm().doRpcCallSync(tlRequestChannelsGetParticipants);
+            System.out.println(JSONObject.toJSONString(tlChannelParticipants));
+            System.out.println(tlChannelParticipants.getParticipants().size());
+            System.out.println(tlChannelParticipants.getParticipants().get(0).getClassId());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (RpcException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
